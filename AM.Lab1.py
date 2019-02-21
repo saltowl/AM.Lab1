73d86b33974e6b4499e4c5c6c5fe1f82bfd452f7
Zalkin <Zalkin.viktor@mail.ru>
2019-02-21 11:41:24 +0300

    Add get_input_data() to read data from file

--- AM.Lab1.py
+++ AM.Lab1.py
@@ -1,1 +1,16 @@
+def get_input_data(file_name):
+	with open(file_name, "r") as file:
+		input_data = []
+		for line in file:
+			try:
+				input_data += map(float, line.replace(",", ".").split())
+			except ValueError: #if not numbers in line
+				pass
+	return input_data
 
+def main():
+	input_data = get_input_data("input.txt")
+
+	pass
+
+main()
